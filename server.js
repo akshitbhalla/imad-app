bb57dc1279ad3b582fd37f217d9a4e8b0343911d
akshitbhalla <akshitbhalla@users.noreply.github.com>
2017-08-08 18:15:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res) {
-    res.sendFile('The first article would be served here.')
+    res.send('The first article would be served here.')
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
