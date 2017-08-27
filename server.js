a01d77fc8952c2485df93fdbc68d55bf0f606e5f
Akshit Bhalla <akshitbhalla@users.noreply.github.com>
2017-08-27 17:25:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user: 'akshitbhalla13',
     database: 'akshitbhalla13',
     host: 'http://db.imad.hasura-app.io',
-    port: 5532,
+    port: 5432,
     password: process.env.DB_PASSWORD
 };
 
